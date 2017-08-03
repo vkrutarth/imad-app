3410b8c506009951b76eccb17d71b2dd45ac160a
Krutarth Vanesa <30493871+vkrutarth@users.noreply.github.com>
2017-08-03 18:18:43 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,6 +5,14 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+
+var content
+
+
+
+
+
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
