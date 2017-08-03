0ab267c7c9664bd3ade3a150c0fb3187d6273b02
Krutarth Vanesa <30493871+vkrutarth@users.noreply.github.com>
2017-08-03 15:37:20 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,7 +8,15 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-
+app.get('/article-one',function(req,res){
+    res.send("Article-One will be coming up here");
+});
+app.get('/article-two',function(req,res){
+    res.send("Article-Two will be coming up here");
+});
+app.get('/article-three',function(req,res){
+    res.send("Article-Three will be coming up here");
+});
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
