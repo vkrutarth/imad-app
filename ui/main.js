e57f0b60beec41e2eea95502f52aa1d5cb78719e
Krutarth Vanesa <30493871+vkrutarth@users.noreply.github.com>
2017-08-03 21:57:03 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,17 @@
 console.log('Loaded!');
+
+
+var element=document.getElementById('main-text');
+element.innerHTML='new value';
+
+
+var img=document.getElementById('madi');
+var d=0;
+function movedown(){
+    d=d+1;
+    img.style.marginBottom=d+"px";
+}
+
+img.onclick=function() {
+    var time=setInterval(movedown,20);
+};
