6c53a68b85362b87f39a9cfc6584c3fa764fa0ff
Krutarth Vanesa <30493871+vkrutarth@users.noreply.github.com>
2017-08-03 17:40:36 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 app.get('/article-one',function(req,res){
-    res.send("Article-One will be coming up here");
+   res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
 });
 app.get('/article-two',function(req,res){
     res.send("Article-Two will be coming up here");
