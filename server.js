648c46d14a1e217f96bc6cad74a7c2fafc6e3754
Krutarth Vanesa <30493871+vkrutarth@users.noreply.github.com>
2017-08-03 19:22:51 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -54,9 +54,9 @@ function createTemplate(data){
               ${title}
           </title>
           <meta name="viewport" content="width-device-width,initial-scale=1" />
-           <style>
-             <link href="/ui/style.css" rel="stylesheet" />
-          </style>
+           
+            <link href="/ui/style.css" rel="stylesheet" />
+          
       </head>
       <body>
           <div class="app">
