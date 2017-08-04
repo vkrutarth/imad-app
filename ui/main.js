ace7c31652d5cc110d6fedbcc012bc9edb7a0b41
Krutarth Vanesa <30493871+vkrutarth@users.noreply.github.com>
2017-08-04 16:59:46 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -11,6 +11,33 @@ button.onclick=function () {
         }
     }
   };
-  request.open('GET','http://vanesam.imad.hasura-app.io/count',true);
+  request.open('GET','http://localhost:8080/count',true);
   request.send(null);
+};
+
+
+
+var submit=document.getElementById('submit');
+submit.onclick=function () {
+
+  var request=new XMLHttpRequest();
+  request.onreadystatechange=function () {
+    if(request.readyState===XMLHttpRequest.DONE){
+        if(request.status===200){
+          var names=request.responseText;
+          names=JSON.parse(names);
+          var list='';
+          for(var i=0;i<name.length;i++){
+            list += '<li>'+names[i]+'</li>';
+          }
+          var ul=document.getElementById('namelist');
+          ul.innerHTML=list;
+        }
+    }
+  };
+  var nameInput=document.getElementById('name');
+  var name=nameInput.value;
+  request.open('GET','http://localhost:8080/submit-name?name='+name,true);
+  request.send(null);
+
 };
