5c544dadca1dc304b397e1a1507b452d37721ce7
Krutarth Vanesa <30493871+vkrutarth@users.noreply.github.com>
2017-08-04 17:03:41 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -28,7 +28,7 @@ submit.onclick=function () {
           names=JSON.parse(names);
           var list='';
           for(var i=0;i<name.length;i++){
-            list == '<li>'+names[i]+'</li>';
+            list += '<li>'+names[i]+'</li>';
           }
           var ul=document.getElementById('namelist');
           ul.innerHTML=list;
